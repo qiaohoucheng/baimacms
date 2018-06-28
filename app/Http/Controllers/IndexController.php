142923083e9723747ccd873dede56cb546f496e8
qiaohoucheng <qiaohoucheng@sina.com>
2018-06-28 15:07:46 +0800

    add qclond

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Qcloud\Cos\Client;
+class IndexController extends Controller
+{
+    //
+    public function __construct()
+    {
+
+    }
+
+    public function index(Request $request)
+    {
+
+    }
+
+    public function upload()
+    {
+
+    }
+}
