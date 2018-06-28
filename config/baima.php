4aeec091b4060b066a165769fe52a59d0867518a
qiaohoucheng <qiaohoucheng@sina.com>
2018-06-28 15:33:10 +0800

    delete baima.php

--- config/baima.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    //腾讯云
-    'cos_appId'=>'1251775319',
-    'cos_secretId'=>'AKIDhHhvxnZwzwBQcfFP8h3nPq6ASYSEdGZq',
-    'cos_secretKey'=>'PiuxpWAiDXffT5WnslS0MWYP3Xr12rUw',
-];
