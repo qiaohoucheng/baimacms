3fa1632374f447552c90baeee45d9767a7319f42
qiaohoucheng <qiaohoucheng@sina.com>
2018-07-13 11:46:38 +0800

    laravel 5.5

---
+++ config/baima.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    //腾讯云
+    'cos_appId'=>'1251775319',
+    'cos_secretId'=>'AKIDhHhvxnZwzwBQcfFP8h3nPq6ASYSEdGZq',
+    'cos_secretKey'=>'PiuxpWAiDXffT5WnslS0MWYP3Xr12rUw',
+    'cos_region'=>'ap-chengdu',
+    'cos_bucket'=>'saber',
+];
