ea6c1084d15b692c0cf18c95a2a8993c22b15f39
qiaohoucheng <qiaohoucheng@sina.com>
2018-07-16 18:37:44 +0800

    css update

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -5,11 +5,11 @@
             <i class="layui-icon layui-icon-shrink-right" id="LAY_app_flexible"></i>
         </a>
     </li>
-    <!--<li class="layui-nav-item layui-this layui-hide-xs layui-hide-sm layui-show-md-inline-block">
+    <li class="layui-nav-item layui-this layui-hide-xs layui-hide-sm layui-show-md-inline-block">
       <a lay-href="" title="">
         控制台
       </a>
-    </li>-->
+    </li>
     <li class="layui-nav-item layui-hide-xs" lay-unselect="">
         <a href="http://www.layui.com/admin/" target="_blank" title="前台">
             <i class="layui-icon layui-icon-website"></i>
@@ -23,10 +23,10 @@
     <li class="layui-nav-item layui-hide-xs" lay-unselect="">
         <input type="text" placeholder="搜索..." autocomplete="off" class="layui-input layui-input-search" layadmin-event="serach" lay-action="template/search/keywords=">
     </li>
-    <span class="layui-nav-bar" style="left: 288px; top: 48px; width: 0px; opacity: 0;"></span></ul>
-<ul class="layui-nav layui-layout-right" lay-filter="layadmin-layout-right">
-
-    <li class="layui-nav-item" lay-unselect="">
+    <span class="layui-nav-bar"></span>
+</ul>
+<ul class="layui-nav layui-layout-right" >
+    <li class="layui-nav-item" >
         <a lay-href="app/message/" layadmin-event="message">
             <i class="layui-icon layui-icon-notice"></i>
 
@@ -39,17 +39,17 @@
 
         </a>
     </li>
-    <li class="layui-nav-item layui-hide-xs" lay-unselect="">
+    <li class="layui-nav-item layui-hide-xs">
         <a href="javascript:;" layadmin-event="theme">
             <i class="layui-icon layui-icon-theme"></i>
         </a>
     </li>
-    <li class="layui-nav-item layui-hide-xs" lay-unselect="">
+    <li class="layui-nav-item layui-hide-xs">
         <a href="javascript:;" layadmin-event="note">
             <i class="layui-icon layui-icon-note"></i>
         </a>
     </li>
-    <li class="layui-nav-item layui-hide-xs" lay-unselect="">
+    <li class="layui-nav-item layui-hide-xs">
         <a href="javascript:;" layadmin-event="fullscreen">
             <i class="layui-icon layui-icon-screen-full"></i>
         </a>
@@ -65,13 +65,27 @@
                 <hr>
                 <dd layadmin-event="logout" style="text-align: center;"><a>退出</a></dd>
             </dl>
-        </script> <a href="javascript:;"> <cite>贤心</cite> <span class="layui-nav-more"></span></a> <dl class="layui-nav-child layui-anim layui-anim-upbit"> <dd><a lay-href="set/user/info">基本资料</a></dd> <dd><a lay-href="set/user/password">修改密码</a></dd> <hr> <dd layadmin-event="logout" style="text-align: center;"><a>退出</a></dd> </dl>
+        </script>
+        <a href="javascript:;">
+            <cite>贤心</cite>
+            <span class="layui-nav-more"></span>
+        </a>
+        <dl class="layui-nav-child layui-anim layui-anim-upbit">
+            <dd><a lay-href="set/user/info">基本资料</a></dd>
+            <dd><a lay-href="set/user/password">修改密码</a></dd>
+            <hr>
+            <dd layadmin-event="logout" style="text-align: center;"><a>退出</a></dd>
+        </dl>
     </li>
-
     <li class="layui-nav-item layui-hide-xs" lay-unselect="">
-        <a href="javascript:;" layadmin-event="about"><i class="layui-icon layui-icon-more-vertical"></i></a>
+        <a href="javascript:;" layadmin-event="about">
+            <i class="layui-icon layui-icon-more-vertical"></i>
+        </a>
     </li>
     <li class="layui-nav-item layui-show-xs-inline-block layui-hide-sm" lay-unselect="">
-        <a href="javascript:;" layadmin-event="more"><i class="layui-icon layui-icon-more-vertical"></i></a>
+        <a href="javascript:;" layadmin-event="more">
+            <i class="layui-icon layui-icon-more-vertical"></i>
+        </a>
     </li>
-    <span class="layui-nav-bar" style="left: 140px; top: 48px; width: 0px; opacity: 0;"></span></ul>
+    <span class="layui-nav-bar"></span>
+</ul>
