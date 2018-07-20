c2562db6bddce70b8d380f2c176f847370a4e428
qiaohoucheng <qiaohoucheng@sina.com>
2018-07-20 12:21:18 +0800

    add layadmin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -29,11 +29,51 @@
         @include('admin.layouts.footer')
     </div>
 </div>
+<div class="layui-layer-shade" id="layui-layer-shade2" times="2" style="z-index: 19891015; background-color: rgb(0, 0, 0); opacity: 0.1;"></div>
+<div class="layui-layer layui-layer-page layui-anim layui-anim-rl layui-layer-adminRight" id="layui-layer2" type="page" times="2" showtime="0" contype="string" style="z-index: 19891016; width: 300px; top: 25px; left: 1620px;">
+<div id="LAY_adminPopupAbout" class="layui-layer-content">
+    <div class="layui-card-header">公告</div>
+    <div class="layui-card-body layui-text layadmin-about">
+        <blockquote class="layui-elem-quote" style="border: none;">
+            layuiAdmin 受国家计算机软件著作权保护（登记号：2018SR410669），未经官网正规渠道授权擅自公开产品源文件、以及直接对产品二次出售的，我们将保留追究法律责任的权利。
+        </blockquote>
+    </div>
+</div>
+</div>
 <script>
+    layui.config({
+        base: '/layui/layadmin/' //指定 layuiAdmin 项目路径
+        ,version: '1.0.0'
+    }).use('index',function(){
+        $('#layui-layer2').hide();
+        $('#layui-layer-shade2').hide();
+    });
     //JavaScript代码区域
     layui.use('element', function(){
         var element = layui.element;
     });
+    layui.use(['jquery','layer'],function (e) {
+        $('#layui-layer-shade2').on('click',function(){
+            $('#layui-layer2').hide();
+            $(this).hide();
+        })
+    })
+    //测试 onevent event
+    layui.use(['jquery','util'],function($,util){
+        var active = {
+            about:function(){
+                layer.event("about","pop",{
+                    'id':1,
+                    'name':'测试',
+                })
+            }
+        }
+        layui.onevent("about", "pop", function (params) {
+            alert(1);
+            $('#layui-layer2').show();
+            $('#layui-layer-shade').show();
+        });
+    })
 </script>
 </body>
 </html>
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,27 +1,27 @@
 <!-- 头部区域（可配合layui已有的水平导航） -->
 <ul class="layui-nav layui-layout-left">
-    <li class="layui-nav-item" >
+    <li class="layui-nav-item" lay-unselect >
         <a href="javascript:;" layadmin-event="flexible" title="侧边伸缩">
             <i class="layui-icon layui-icon-shrink-right" id="LAY_app_flexible"></i>
         </a>
     </li>
-    <li class="layui-nav-item layui-hide-xs" >
+    <li class="layui-nav-item layui-hide-xs" lay-unselect >
         <a href="http://www.layui.com/admin/" target="_blank" title="前台">
             <i class="layui-icon layui-icon-website"></i>
         </a>
     </li>
-    <li class="layui-nav-item" >
+    <li class="layui-nav-item" lay-unselect>
         <a href="javascript:;" layadmin-event="refresh" title="刷新">
             <i class="layui-icon layui-icon-refresh-3"></i>
         </a>
     </li>
-    <li class="layui-nav-item ">
+    <li class="layui-nav-item " lay-unselect>
         <input type="text" placeholder="搜索..." autocomplete="off" class="layui-input layui-input-search" layadmin-event="serach">
     </li>
 </ul>
 <ul class="layui-nav layui-layout-right">
-    <li class="layui-nav-item" >
-        <a lay-href="app/message/" layadmin-event="message">
+    <li class="layui-nav-item" lay-unselect>
+        <a lay-href="app/message/" layadmin-event="message" >
             <i class="layui-icon layui-icon-notice"></i>
             <!-- 如果有新消息，则显示小圆点 -->
             <script type="text/html" template="" lay-url="./json/message/new.js">
@@ -31,7 +31,7 @@
             </script>
         </a>
     </li>
-    <li class="layui-nav-item">
+    <li class="layui-nav-item" lay-unselect>
         <a href="javascript:;">
             <i class="layui-icon layui-icon-screen-full"></i>
         </a>
@@ -54,8 +54,8 @@
             </dd>
         </dl>
     </li>
-    <li class="layui-nav-item">
-        <a href="javascript:;">
+    <li class="layui-nav-item" lay-unselect>
+        <a href="javascript:;" lay-event="about">
             <i class="layui-icon layui-icon-more-vertical"></i>
         </a>
     </li>
