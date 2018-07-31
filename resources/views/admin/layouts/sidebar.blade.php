02b063fcbd98da4b66a50d52ba2300aa153eefca
qiaohoucheng <qiaohoucheng@sina.com>
2018-07-31 12:20:09 +0800

    update sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -5,14 +5,19 @@
             <span>CMS</span>
         </div>
         <ul class="layui-nav layui-nav-tree" lay-shrink="all" id="LAY-system-side-menu" >
-            <li class="layui-nav-item layui-nav-itemed">
-                <a href="javascript:;" lay-tips="主页" lay-direction="2">
-                    <i class="layui-icon layui-icon-home"></i> <cite>主页</cite>
+            <li class="layui-nav-item layui-this">
+                <a href="javascript:;" >
+                    <i class="layui-icon layui-icon-chart-screen"></i> <cite>概览</cite>
+                </a>
+            </li>
+            <li class="layui-nav-item ">
+                <a href="javascript:;" lay-tips="首页管理" lay-direction="2">
+                    <i class="layui-icon layui-icon-template-1"></i> <cite>首页管理</cite>
                     <span class="layui-nav-more"></span>
                 </a>
                 <dl class="layui-nav-child">
-                    <dd  class="layui-this">
-                        <a href="javascript:;" lay-href="/">控制台</a>
+                    <dd>
+                        <a href="javascript:;" lay-href="/">轮播图管理</a>
                     </dd>
                     <dd  class="">
                         <a href="javascript:;" lay-href="home/homepage1">主页一</a>
@@ -40,8 +45,8 @@
                 </dl>
             </li>
             <li class="layui-nav-item ">
-                <a href="javascript:;" lay-tips="评论管理" lay-direction="2">
-                    <i class="layui-icon layui-icon-dialogue"></i> <cite>评论管理</cite>
+                <a href="javascript:;" lay-tips="分类管理" lay-direction="2">
+                    <i class="layui-icon layui-icon-fonts-code"></i> <cite>分类管理</cite>
                     <span class="layui-nav-more"></span>
                 </a>
                 <dl class="layui-nav-child">
@@ -57,8 +62,59 @@
                 </dl>
             </li>
             <li class="layui-nav-item ">
-                <a href="javascript:;" lay-tips="多媒体管理" lay-direction="2">
-                    <i class="layui-icon layui-icon-set"></i> <cite>多媒体管理</cite>
+                <a href="javascript:;" lay-tips="用户管理" lay-direction="2">
+                    <i class="layui-icon layui-icon-user"></i> <cite>用户管理</cite>
+                    <span class="layui-nav-more"></span>
+                </a>
+                <dl class="layui-nav-child">
+                    <dd>
+                        <a href="javascript:;" lay-href="/">控制台</a>
+                    </dd>
+                    <dd  class="">
+                        <a href="javascript:;" lay-href="home/homepage1">主页一</a>
+                    </dd>
+                    <dd >
+                        <a href="javascript:;" lay-href="home/homepage2">主页二</a>
+                    </dd>
+                </dl>
+            </li>
+            <li class="layui-nav-item ">
+                <a href="javascript:;" lay-tips="资源管理" lay-direction="2">
+                    <i class="layui-icon layui-icon-picture"></i> <cite>资源管理</cite>
+                    <span class="layui-nav-more"></span>
+                </a>
+                <dl class="layui-nav-child">
+                    <dd>
+                        <a href="javascript:;" lay-href="/">控制台</a>
+                    </dd>
+                    <dd  class="">
+                        <a href="javascript:;" lay-href="home/homepage1">主页一</a>
+                    </dd>
+                    <dd >
+                        <a href="javascript:;" lay-href="home/homepage2">主页二</a>
+                    </dd>
+                </dl>
+            </li>
+            <li class="layui-nav-item ">
+                <a href="javascript:;" lay-tips="文档管理" lay-direction="2">
+                    <i class="layui-icon layui-icon-file-b"></i> <cite>文档管理</cite>
+                    <span class="layui-nav-more"></span>
+                </a>
+                <dl class="layui-nav-child">
+                    <dd>
+                        <a href="javascript:;" lay-href="/">控制台</a>
+                    </dd>
+                    <dd  class="">
+                        <a href="javascript:;" lay-href="home/homepage1">主页一</a>
+                    </dd>
+                    <dd >
+                        <a href="javascript:;" lay-href="home/homepage2">主页二</a>
+                    </dd>
+                </dl>
+            </li>
+            <li class="layui-nav-item ">
+                <a href="javascript:;" lay-tips="权限管理" lay-direction="2">
+                    <i class="layui-icon layui-icon-senior"></i> <cite>权限管理</cite>
                     <span class="layui-nav-more"></span>
                 </a>
                 <dl class="layui-nav-child">
