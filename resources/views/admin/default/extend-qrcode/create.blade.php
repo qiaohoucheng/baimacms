6eb91e8a9c239a70a72d98047104542491e5753f
qiaohoucheng <qiaohoucheng@sina.com>
2018-08-07 17:15:44 +0800

    qrcode 0.1 finish

--- resources/views/admin/default/extend-qrcode/create.blade.php
+++ resources/views/admin/default/extend-qrcode/create.blade.php
@@ -42,7 +42,7 @@
                             <div class="layui-form-item">
                                 <label class="layui-form-label">大小</label>
                                 <div class="layui-input-block">
-                                    <input type="text" name="size" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" value="300" disabled>
+                                    <input type="number" name="size" lay-verify="required" placeholder="请输入" autocomplete="off" class="layui-input" value="300" disabled>
                                 </div>
                             </div>
                             <div class="layui-form-item">
@@ -60,13 +60,12 @@
                             <div class="layui-form-item">
                                 <label class="layui-form-label">边距</label>
                                 <div class="layui-input-block">
-                                    <input type="text" name="margin" lay-verify="required" placeholder="请输入边距" autocomplete="off" class="layui-input">
+                                    <input type="number" name="margin" lay-verify="required" placeholder="请输入边距" autocomplete="off" class="layui-input" value="1">
                                 </div>
                             </div>
                             <div class="layui-form-item">
                                 <div class="layui-input-block">
                                     <button class="layui-btn" lay-submit=""  lay-filter="create-btn">立即提交</button>
-                                    <button  type="button" laybaima-event="look" class="layui-btn layui-btn-primary">预览</button>
                                     <button type="reset" class="layui-btn layui-btn-primary">重置</button>
                                 </div>
                             </div>
@@ -77,14 +76,13 @@
             <div class="layui-col-md4">
                 <div class="layui-card">
                     <div class="layui-card-header">
-                        <h3>预览</h3>
+                        <h3>二维码</h3>
                     </div>
                     <div class="layui-card-body">
-                        <div class="layui-images" style="width: 100%; height: 486px;text-align: center;padding:auto 0;">
+                        <div class="layui-images " style="width: 100%; text-align: center;height: 486px;padding:auto 0;">
                             <div class="qr-box" style="padding-top:50px;">
-                                {!! QrCode::size(300)->generate(Request::url()) !!}
+                                {!! QrCode::size(300)->color(255,0,255)->generate('http://qiaohoucheng.com') !!}
                             </div>
-                            <button type="download"  class="layui-btn layui-btn-primary">下载</button>
                         </div>
                     </div>
                 </div>
@@ -117,11 +115,8 @@
             //监听提交
             form.on('submit(create-btn)', function(data){
                 $.post(data.form.action,data.field,function(data){
-                    layer.msg(data.message);
                     if(data.code==1){
-                        //setTimeout(function(){
-                        //    window.location.href='/power-menu';
-                        //},1000);
+                         $('.qr-box').html('<img src='+data.pic_url+'?'+Math.random()+'>');
                     }
                 });
                 return false;
--- resources/views/admin/default/home/index.blade.php
+++ resources/views/admin/default/home/index.blade.php
@@ -2,7 +2,4 @@
 @section('css')
 @endsection
 @section('content')
-    <div class="visible-print text-center">
-        {!! QrCode::size(100)->color(255,0,255)->generate(Request::url()) !!}
-    </div>
 @endsection
